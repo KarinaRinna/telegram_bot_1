599bf25ba61995a019f30b2b58730e13ee2b11e3
Mandarin <zurual@mail.ru>
2023-07-03 12:08:44 +0300

    add site

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('6343220717:AAFwAAD-s2nmRnrVFPUiwRK40RdvW2Lbt1g')
 
@@ -9,7 +10,7 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
-@bot.message_handler()
+@bot.message_handler(content_types=['text'])
 def get_user_text(message):
     if message.text == "Hello":
         bot.send_message(message.chat.id, "И тебе привет", parse_mode='html')
@@ -21,8 +22,30 @@ def get_user_text(message):
     else:
         bot.send_message(message.chat.id, "Я тебя не понимаю", parse_mode='html')
 
+@bot.message_handler(content_types=['photo'])
+def get_user_photo(message):
+    bot.send_message(message.chat.id, 'Вау, крутое фото')
+
+
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Посетить веб сайт', url='https://genshin.hoyoverse.com/ru/home'))
+    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    website = types.KeyboardButton('Веб сайт')
+    start = types.KeyboardButton('Start')
+    markup.add(website, start)
+    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
+
+
+
 
 bot.polling(none_stop=True)
 
 
-# 18:00
+# https://www.youtube.com/watch?v=HodO2eBEz_8&list=PL0lO_mIqDDFUdlTc097-1A9IBchtJEggp
